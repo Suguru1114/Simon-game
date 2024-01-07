5fe4baa3c102da433bcd4e5bcdbc086c864718fc
Suguru Miyata <suguru1114@gmail.com>
2024-01-07 12:46:30 +1100

    Initial commit

---
+++ game.js
@@ -0,0 +1,9 @@
+// check if Gamepad.js is linked
+// $("h1").css("color", "red");
+// alert("hello");
+
+// randomNumber 1 to 3 
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*3 ) +1;
+    console.log(randomNumber);
+}
