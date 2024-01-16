10f8f58e1ab5f3ddde0474572b77fcfbb602c2f8
Suguru Miyata <suguru1114@gmail.com>
2024-01-16 22:24:02 +1100

    fix handler function

--- game.js
+++ game.js
@@ -3,27 +3,28 @@
 // alert("hello");
 
 // randomNumber 1 to 4 to pick 4 color  
+var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+// handler function
+$(".btn").on("click", function(){
+
+    var userChosenColour = $(this).attr("id");
+    // can add this id to userChosenColour 
+    console.log("Button ID" + this.id);
+
+    
+    // alert("the button is clicked");
+});
+
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4 );
-
-
     var randomChosenColor = buttonColors[randomNumber];
-        gamePattern.push(randomChosenColor);
+    gamePattern.push(randomChosenColor);
 
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    // handler function
-    $("btn").on("click", function(){
-
-        var userChosenColour = $(this).attr("id");
-        // can add this id to userChosenColour 
-        console.log("Button ID" + this.id);
-
-        
-        alert("the burtton is clicked");
-    });
 
 }
